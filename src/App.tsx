4b29e51c4fc6370002ddeb80e0d6da4214a6e090
Logic <10zimmer10@gmail.com>
2024-08-15 15:26:58 -0700

    Added FFXIV server util enums, added UI elements to select world and data center

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
 
-import { IconButton } from "@mui/material";
+import {
+  FormControl,
+  IconButton,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+} from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 import TextField from "@mui/material/TextField";
 
 import "./App.css";
 
 function App() {
+  const [dataCenter, setDataCenter] = React.useState("");
+  const [homeWorld, setHomeWorld] = React.useState("");
   const [searchText, setSearchText] = useState("");
 
   // Allow for search bar submission via enter press
@@ -22,21 +31,65 @@ function App() {
   }
 
   return (
-    <div className="search-container">
-      <TextField
-        id="search-input"
-        onChange={(e) => {
-          setSearchText(e.target.value);
-        }}
-        onKeyDown={handleKeyDown}
-        placeholder="Search for an item"
-        value={searchText}
-        variant="outlined"
-      />
-      <IconButton color="secondary" onClick={searchItem}>
-        <SearchIcon />
-      </IconButton>
-    </div>
+    <>
+      <div id="top-nav">
+        <div id="search-container" className="nav-item">
+          <TextField
+            id="search-input"
+            onChange={(e) => {
+              setSearchText(e.target.value);
+            }}
+            onKeyDown={handleKeyDown}
+            placeholder="Search for an item"
+            value={searchText}
+            variant="outlined"
+          />
+          <IconButton color="secondary" onClick={searchItem}>
+            <SearchIcon />
+          </IconButton>
+        </div>
+        <div className="flex">
+          <div className="dropdown-button flex">
+            <FormControl sx={{ m: 1, minWidth: 160 }}>
+              <InputLabel id="data-center-select-helper-label">
+                Data Center
+              </InputLabel>
+              <Select
+                labelId="data-center-select-helper-label"
+                value={dataCenter}
+                label="Data Center"
+                onChange={(event: SelectChangeEvent) =>
+                  setDataCenter(event.target.value)
+                }
+              >
+                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+              </Select>
+            </FormControl>
+          </div>
+          <div className="dropdown-button flex">
+            <FormControl disabled={!dataCenter} sx={{ m: 1, minWidth: 160 }}>
+              <InputLabel id="home-world-select-helper-label">
+                Home World
+              </InputLabel>
+              <Select
+                labelId="home-world-select-helper-label"
+                value={homeWorld}
+                label="Home World"
+                onChange={(event: SelectChangeEvent) =>
+                  setHomeWorld(event.target.value)
+                }
+              >
+                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+              </Select>
+            </FormControl>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/utils/servers.tsx
@@ -0,0 +1,102 @@
+// TODO: Pull server info from API
+
+// TODO: Expand beyond NA datacenters
+export enum DATA_CENTERS_ENUM {
+  AETHER = "Aether",
+  CRYSTAL = "Crystal",
+  DYNAMIS = "Dynamis",
+  PRIMAL = "Primal",
+}
+export const DATA_CENTERS = Object.values(DATA_CENTERS_ENUM);
+
+export enum WORLDS_ENUM {
+  ADAMANTOISE = "Adamantoise",
+  CACTUAR = "Cactuar",
+  FAERIE = "Faerie",
+  GILGAMESH = "Gilgamesh",
+  JENOVA = "Jenova",
+  MIDGARDSORMR = "Midgardsormr",
+  SARGATANAS = "Sargatanas",
+  SIREN = "Siren",
+  BALMUNG = "Balmung",
+  BRYNHILDR = "Brynhildr",
+  COEURL = "Coeurl",
+  DIABOLOS = "Diabolos",
+  GOBLIN = "Goblin",
+  MALBORO = "Malboro",
+  MATEUS = "Mateus",
+  ZALERA = "Zalera",
+  CUCHULAINN = "Cuchulainn",
+  GOLEM = "Golem",
+  HALICARNASSUS = "Halicarnassus",
+  KRAKEN = "Kraken",
+  MADUIN = "Maduin",
+  MARILITH = "Marilith",
+  RAFFLESIA = "Rafflesia",
+  SERAPH = "Seraph",
+  BEHEMOTH = "Behemoth",
+  EXCALIBUR = "Excalibur",
+  EXODUS = "Exodus",
+  FAMFRIT = "Famfrit",
+  HYPERION = "Hyperion",
+  LAMIA = "Lamia",
+  LEVIATHAN = "Leviathan",
+  ULTROS = "Ultros",
+}
+export const WORLDS = Object.values(WORLDS_ENUM);
+
+// TODO: Expand beyond NA worlds
+export const SERVERS = [
+  {
+    dataCenter: DATA_CENTERS_ENUM.AETHER,
+    worlds: [
+      WORLDS_ENUM.ADAMANTOISE,
+      WORLDS_ENUM.CACTUAR,
+      WORLDS_ENUM.FAERIE,
+      WORLDS_ENUM.GILGAMESH,
+      WORLDS_ENUM.JENOVA,
+      WORLDS_ENUM.MIDGARDSORMR,
+      WORLDS_ENUM.SARGATANAS,
+      WORLDS_ENUM.SIREN,
+    ],
+  },
+  {
+    dataCenter: DATA_CENTERS_ENUM.CRYSTAL,
+    worlds: [
+      WORLDS_ENUM.BALMUNG,
+      WORLDS_ENUM.BRYNHILDR,
+      WORLDS_ENUM.COEURL,
+      WORLDS_ENUM.DIABOLOS,
+      WORLDS_ENUM.GOBLIN,
+      WORLDS_ENUM.MALBORO,
+      WORLDS_ENUM.MATEUS,
+      WORLDS_ENUM.ZALERA,
+    ],
+  },
+  {
+    dataCenter: DATA_CENTERS_ENUM.DYNAMIS,
+    worlds: [
+      WORLDS_ENUM.CUCHULAINN,
+      WORLDS_ENUM.GOLEM,
+      WORLDS_ENUM.HALICARNASSUS,
+      WORLDS_ENUM.KRAKEN,
+      WORLDS_ENUM.MADUIN,
+      WORLDS_ENUM.MARILITH,
+      WORLDS_ENUM.RAFFLESIA,
+      WORLDS_ENUM.SERAPH,
+    ],
+  },
+  {
+    dataCenter: DATA_CENTERS_ENUM.PRIMAL,
+    worlds: [
+      WORLDS_ENUM.BEHEMOTH,
+      WORLDS_ENUM.EXCALIBUR,
+      WORLDS_ENUM.EXODUS,
+      WORLDS_ENUM.FAMFRIT,
+      WORLDS_ENUM.HYPERION,
+      WORLDS_ENUM.LAMIA,
+      WORLDS_ENUM.LEVIATHAN,
+      WORLDS_ENUM.ULTROS,
+    ],
+  },
+];
